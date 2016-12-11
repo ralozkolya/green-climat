c5925faa5ea7db7b80ca6a7886a1e705ea2c2df0
Kolya <raloz1@gmail.com>
2016-12-11 05:01:38 +0400

    Restructurisation
    Add navigation

--- www/application/controllers/Site.php
+++ www/application/controllers/Site.php
@@ -3,8 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Site extends MY_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->data['navigation'] = $this->get_navigation();
+	}
+
 	public function index() {
+		$this->data['highlighted'] = 'home';
 		$this->view('pages/home');
+	}
+
+	private function get_navigation() {
+		$this->load->model('Page');
+		return $this->Page->get_navigation();
 	}
 
 }
---
+++ www/application/views/elements/header.php
@@ -0,0 +1,26 @@
+<header class="header text-center">
+	<div class="lang">
+		<?php foreach(get_alternative_langs() as $lang): ?>
+			<a class="unstyled" href="<?php echo lang_link($lang); ?>"><?php echo get_lang_label($lang); ?></a>
+		<?php endforeach; ?>
+	</div>
+	<div>
+		<a href="<?php echo locale_url(); ?>" class="unstyled"><img src="<?php echo static_url('img/logo.png?v='.V); ?>" alt="Logo"></a>
+	</div>
+	<div class="text-center">
+		<ul class="navigation">
+			<?php foreach($navigation as $n): ?>
+				<li>
+					<?php
+						$class = 'unstyled';
+
+						if($highlighted === $n->slug) $class .= ' active';
+					?>
+					<a class="<?php echo $class; ?>"
+						href="<?php echo locale_url($n->slug); ?>"><?php echo $n->title; ?>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+</header>
