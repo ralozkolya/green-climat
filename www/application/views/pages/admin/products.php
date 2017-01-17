194b99c9aa64a1e8647bb9170d8097f353c5a43a
Nikoloz Razmadze <raloz1@gmail.com>
2017-01-17 13:21:13 +0400

    Add "Under Construction..." to products page

---
+++ www/application/views/pages/admin/products.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php $this->load->view('elements/admin/head'); ?>
+	<link rel="stylesheet" href="<?php echo static_url('css/admin/banners.css?v='.V); ?>">
+</head>
+<body>
+	<?php $this->load->view('elements/admin/sidebar'); ?>
+	<div class="content">
+		<h3>Under construction...</h3>
+	</div>
+</body>
+</html>
