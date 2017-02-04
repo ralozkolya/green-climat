9a1ac6df4f2d32b27af9f9717c156b858fa8ca1a
Kolya <raloz1@gmail.com>
2017-02-05 01:38:40 +0400

    Add tawk.to chat

--- www/application/views/elements/footer.php
+++ www/application/views/elements/footer.php
@@ -19,3 +19,16 @@
   ga('create', 'UA-46276983-6', 'auto');
   ga('send', 'pageview');
 </script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/589647c1fd0c4f0a061b456d/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
