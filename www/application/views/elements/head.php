dcef3e5e9b2b0b30660cb2fbc6895e4bd073996b
Kolya <raloz1@gmail.com>
2017-02-05 00:51:04 +0400

    Add og tags

--- www/application/views/elements/head.php
+++ www/application/views/elements/head.php
@@ -7,3 +7,16 @@
 <link rel="icon" type="image/png" href="<?php echo static_url('img/favicon.png'); ?>">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="<?php echo static_url('js/general.js?v='.V); ?>"></script>
+<meta property="og:url" content="<?php echo current_url(); ?>">
+<meta property="og:type" content="website">
+<?php if(empty($item)): ?>
+	<meta property="og:title" content="Green Climat">
+<?php else: ?>
+	<?php $title = $item->name ? $item->name : $item->title; ?>
+	<meta property="og:title" content="<?php echo $title; ?>">
+<?php endif; ?>
+<?php if(empty($gallery)): ?>
+	<meta property="og:image" content="<?php echo static_url('img/logo_og.png'); ?>">
+<?php else: ?>
+	<meta property="og:image" content="<?php echo "{$uploads_path}/{$gallery[0]->image}"; ?>">
+<?php endif; ?>
