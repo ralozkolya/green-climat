7c502388b7d36ba7c5b7816b544cec458ea013cc
Kolya <raloz1@gmail.com>
2017-02-22 22:57:27 +0400

    Add DB structure

---
+++ green_climat.sql
@@ -0,0 +1,216 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.19-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5135
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table green_climat.admins
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.banners
+CREATE TABLE IF NOT EXISTS `banners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image` varchar(256) NOT NULL,
+  `link` varchar(512) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `blank` bit(1) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_name` varchar(256) NOT NULL,
+  `en_name` varchar(256) NOT NULL,
+  `ru_name` varchar(256) NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `parent` int(11) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.news
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_title` varchar(200) NOT NULL,
+  `en_title` varchar(200) NOT NULL,
+  `ru_title` varchar(200) NOT NULL,
+  `ka_body` text NOT NULL,
+  `en_body` text NOT NULL,
+  `ru_body` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `priority` (`priority`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.pages
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_title` varchar(256) NOT NULL,
+  `en_title` varchar(256) NOT NULL,
+  `ru_title` varchar(256) NOT NULL,
+  `ka_body` text NOT NULL,
+  `en_body` text NOT NULL,
+  `ru_body` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `navigation` bit(1) NOT NULL DEFAULT b'0',
+  `editable` bit(1) NOT NULL DEFAULT b'0',
+  `priority` int(11) NOT NULL DEFAULT '0',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.partners
+CREATE TABLE IF NOT EXISTS `partners` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_title` varchar(200) NOT NULL,
+  `en_title` varchar(200) NOT NULL,
+  `ru_title` varchar(200) NOT NULL,
+  `ka_body` text NOT NULL,
+  `en_body` text NOT NULL,
+  `ru_body` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `priority` (`priority`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.partner_images
+CREATE TABLE IF NOT EXISTS `partner_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`),
+  CONSTRAINT `FK_partner_images_partners` FOREIGN KEY (`item`) REFERENCES `partners` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.post_images
+CREATE TABLE IF NOT EXISTS `post_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`),
+  CONSTRAINT `FK_post_images_news` FOREIGN KEY (`item`) REFERENCES `news` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_name` varchar(200) NOT NULL,
+  `en_name` varchar(200) NOT NULL,
+  `ru_name` varchar(200) NOT NULL,
+  `ka_desc` text NOT NULL,
+  `en_desc` text NOT NULL,
+  `ru_desc` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `category` int(11) NOT NULL,
+  `top` bit(1) NOT NULL DEFAULT b'0',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.product_images
+CREATE TABLE IF NOT EXISTS `product_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `product` (`item`),
+  CONSTRAINT `FK_product_images_products` FOREIGN KEY (`item`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.projects
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_title` varchar(200) NOT NULL,
+  `en_title` varchar(200) NOT NULL,
+  `ru_title` varchar(200) NOT NULL,
+  `ka_body` text NOT NULL,
+  `en_body` text NOT NULL,
+  `ru_body` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `priority` (`priority`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.project_images
+CREATE TABLE IF NOT EXISTS `project_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `item` (`item`),
+  CONSTRAINT `FK__projects` FOREIGN KEY (`item`) REFERENCES `projects` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+
+-- Dumping structure for table green_climat.services
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ka_title` varchar(200) NOT NULL,
+  `en_title` varchar(200) NOT NULL,
+  `ru_title` varchar(200) NOT NULL,
+  `ka_body` text NOT NULL,
+  `en_body` text NOT NULL,
+  `ru_body` text NOT NULL,
+  `slug` varchar(200) NOT NULL,
+  `priority` int(11) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `priority` (`priority`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+-- Dumping structure for table green_climat.service_images
+CREATE TABLE IF NOT EXISTS `service_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` int(11) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `service` (`item`),
+  CONSTRAINT `FK_service_images_services` FOREIGN KEY (`item`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
