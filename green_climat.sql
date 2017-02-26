931d28c4e6ccddbaabfad61bae2552548f40f90d
Kolya <raloz1@gmail.com>
2017-02-27 01:45:19 +0400

    New structure

--- green_climat.sql
+++ green_climat.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.1.19-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5135
+-- HeidiSQL Version:             9.4.0.5152
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,6 +10,11 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for green_climat
+CREATE DATABASE IF NOT EXISTS `green_climat` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `green_climat`;
 
 -- Dumping structure for table green_climat.admins
 CREATE TABLE IF NOT EXISTS `admins` (
@@ -20,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `admins` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.banners
 CREATE TABLE IF NOT EXISTS `banners` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,21 +37,22 @@ CREATE TABLE IF NOT EXISTS `banners` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.categories
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ka_name` varchar(256) NOT NULL,
   `en_name` varchar(256) NOT NULL,
   `ru_name` varchar(256) NOT NULL,
+  `image` varchar(512) NOT NULL,
   `slug` varchar(200) NOT NULL,
   `parent` int(11) NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `slug` (`slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.news
 CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -64,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   KEY `priority` (`priority`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.pages
 CREATE TABLE IF NOT EXISTS `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -83,37 +89,24 @@ CREATE TABLE IF NOT EXISTS `pages` (
   UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.partners
 CREATE TABLE IF NOT EXISTS `partners` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ka_title` varchar(200) NOT NULL,
   `en_title` varchar(200) NOT NULL,
   `ru_title` varchar(200) NOT NULL,
-  `ka_body` text NOT NULL,
-  `en_body` text NOT NULL,
-  `ru_body` text NOT NULL,
+  `link` varchar(512) NOT NULL,
+  `image` varchar(512) NOT NULL,
   `slug` varchar(200) NOT NULL,
   `priority` int(11) NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `slug` (`slug`),
   KEY `priority` (`priority`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-
--- Dumping structure for table green_climat.partner_images
-CREATE TABLE IF NOT EXISTS `partner_images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `item` int(11) NOT NULL,
-  `image` varchar(256) NOT NULL,
-  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `item` (`item`),
-  CONSTRAINT `FK_partner_images_partners` FOREIGN KEY (`item`) REFERENCES `partners` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.post_images
 CREATE TABLE IF NOT EXISTS `post_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -125,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `post_images` (
   CONSTRAINT `FK_post_images_news` FOREIGN KEY (`item`) REFERENCES `news` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.products
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -144,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.product_images
 CREATE TABLE IF NOT EXISTS `product_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -156,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `product_images` (
   CONSTRAINT `FK_product_images_products` FOREIGN KEY (`item`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.projects
 CREATE TABLE IF NOT EXISTS `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -174,7 +167,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
   KEY `priority` (`priority`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.project_images
 CREATE TABLE IF NOT EXISTS `project_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -186,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `project_images` (
   CONSTRAINT `FK__projects` FOREIGN KEY (`item`) REFERENCES `projects` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
-
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.services
 CREATE TABLE IF NOT EXISTS `services` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -204,6 +197,7 @@ CREATE TABLE IF NOT EXISTS `services` (
   KEY `priority` (`priority`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
+-- Data exporting was unselected.
 -- Dumping structure for table green_climat.service_images
 CREATE TABLE IF NOT EXISTS `service_images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -214,3 +208,8 @@ CREATE TABLE IF NOT EXISTS `service_images` (
   KEY `service` (`item`),
   CONSTRAINT `FK_service_images_services` FOREIGN KEY (`item`) REFERENCES `services` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
